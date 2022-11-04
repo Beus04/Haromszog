375162794b5b23f1e6a640cf162e0bafb84521bc
porti <porti@DESKTOP-DMJ8EVQ>
2022-11-04 21:11:45 +0100

    Add project files.

---
+++ Haromszog/Haromszog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Haromszog
+{
+    public class Haromszog
+    {
+        private int a = 0;
+        private int b = 0;
+        private int c = 0;
+
+        public Haromszog(int aoldal, int boldal, int coldal)
+        {
+            a = aoldal;
+            b = boldal;
+            c = coldal;
+        }
+
+        public int Kerulet()
+        {
+            return a + b + c;       
+        }
+
+        public double Terulet()
+        {
+            double s = Kerulet() / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+    }
+}
---
+++ Haromszog/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Haromszog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        
+            Console.WriteLine("Kérem adja meg az a oldal hosszát: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem adja meg a b oldal hosszát: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Kérem adja meg a c oldal hosszát: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Haromszog haromszog = new Haromszog(a, b, c);
+
+            Console.WriteLine($"A háromszög kerülete: {haromszog.Kerulet()}");
+
+            Console.WriteLine($"A háromszög területe: {haromszog.Terulet()}");
+
+        }
+    }
+}
